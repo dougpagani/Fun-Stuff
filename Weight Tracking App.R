42dd40329f89899372fab6be76398c945a1ba04d
Dean Mirabito <djmirabito@gmail.com>
2016-06-21 21:29:52 -0400

    Replaced with the real function

---
+++ Weight Tracking App.R
@@ -0,0 +1,26 @@
+#Function to cache entry of weight per day and display a graph
+
+addweight <- function(weight){
+  
+  #caching input
+  odata <- read.csv("C:\\Users\\Dean M\\Desktop\\R Programming\\Fun\\Weight Tracking.csv",
+                    header = TRUE)
+  x <- length(odata$Date) + 1
+  odata[x,1] <- as.character.Date(Sys.Date())
+  odata[x,2] <- weight
+  odata <- subset(odata, is.na(odata$Weight) == FALSE)
+  write.csv(odata, file = "C:\\Users\\Dean M\\Desktop\\R Programming\\Fun\\Weight Tracking.csv", 
+            row.names = FALSE)
+  
+  #graphing complete history
+  library(ggplot2)
+  gdata <- read.csv("C:\\Users\\Dean M\\Desktop\\R Programming\\Fun\\Weight Tracking.csv",
+                    header = TRUE)
+  gdata <- subset(gdata, is.na(gdata$Weight) == FALSE)
+  ggplot(gdata, aes(x=Date, y=Weight)) +
+    geom_point(shape=1) 
+}
+
+
+
+
