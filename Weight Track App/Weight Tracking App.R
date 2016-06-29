b479d3951925920fc40bef1de2e9c73274b92840
Dean Mirabito <djmirabito@gmail.com>
2016-06-28 20:03:20 -0400

    Changed sourcing

--- Weight Track App/Weight Tracking App.R
+++ Weight Track App/Weight Tracking App.R
@@ -3,18 +3,18 @@
 addweight <- function(weight){
   
   #caching input
-  odata <- read.csv("C:\\Users\\Dean\\Documents\\GitHub\\Fun-Stuff\\Weight Tracking.csv",
+  odata <- read.csv("C:\\Users\\Dean\\Documents\\GitHub\\Fun-Stuff\\Weight Track App\\Weight Tracking.csv",
                     header = TRUE)
   x <- length(odata$Date) + 1
   odata[x,1] <- as.character.Date(Sys.Date())
   odata[x,2] <- weight
   odata <- subset(odata, is.na(odata$Weight) == FALSE)
-  write.csv(odata, file = "C:\\Users\\Dean\\Documents\\GitHub\\Fun-Stuff\\Weight Tracking.csv", 
+  write.csv(odata, file = "C:\\Users\\Dean\\Documents\\GitHub\\Fun-Stuff\\Weight Track App\\Weight Tracking.csv", 
             row.names = FALSE)
   
   #graphing complete history
   library(ggplot2)
-  gdata <- read.csv("C:\\Users\\Dean\\Documents\\GitHub\\Fun-Stuff\\Weight Tracking.csv",
+  gdata <- read.csv("C:\\Users\\Dean\\Documents\\GitHub\\Fun-Stuff\\Weight Track App\\Weight Tracking.csv",
                     header = TRUE)
   gdata <- subset(gdata, is.na(gdata$Weight) == FALSE)
   ggplot(gdata, aes(x=Date, y=Weight)) +
