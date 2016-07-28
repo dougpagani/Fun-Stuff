f7a103c455cde4aafdde280c2b1597de07f82b84
Dean Mirabito <djmirabito@gmail.com>
2016-07-28 12:04:45 -0400

    New project
    Campaign spending data is publicly reported! This will be fun

---
+++ Campaign Spending/Fave Food.R
@@ -0,0 +1,11 @@
+#Where does your favorite candidate like to eat?
+#Function to analyze public campaign expenditure data and 
+#determine where that candidate eats the most
+
+#data available at: http://www.fec.gov/disclosurep/PDownload.do
+
+fave_food <- function(){
+  
+  
+  
+}
